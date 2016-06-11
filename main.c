0930ed3b60ba287d01a2a5db6c95e20f423ef570
Мargo <borovaya_rita@mail.ru>
2016-06-11 13:03:26 +0600

    Update main.c

--- main.c
+++ main.c
@@ -8,23 +8,19 @@ int main()
 	int a, b, c;
 	scanf("%d %d %d", &a, &b, &c);
 	long double discriminant = b * b - 4 * a * c;
-	if (a == 0)
-	printf("a = 0;\n");
-	else
+	if (discriminant > 0)
 	{
-		if (discriminant > 0)
-		{
-			printf("D > 0; 2 корня:\n");
-			printf("x1 = %lf\n", (-b + sqrt(discriminant))/(2 * a));
-			printf("x2 = %lf\n", (-b - sqrt(discriminant))/(2 * a));
-		}
-		else if (discriminant == 0)
-		{
-			printf("D = 0; 1 корень:\n");
-			printf("x = %lf", -1.*b / (2 * a));
-		}
-		else if (discriminant < 0)
-			printf("D < 0; нет корней.\n");
+		printf("D > 0; 2 корня:\n");
+		printf("x1 = %lf\n", (-b + sqrt(discriminant))/(2 * a));
+		printf("x2 = %lf\n", (-b - sqrt(discriminant))/(2 * a));
 	}
-		return 0;
+	else if (discriminant == 0)
+	{
+		printf("D = 0; 1 корень:\n");
+		printf("x = %lf", -1.*b / (2 * a));
+	}
+	else if (discriminant < 0)
+		printf("D < 0; нет корней.\n");
+	}
+	return 0;
 }
